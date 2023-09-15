c689343a07870088aab76eb3ac374ceb7e9a8834
Mauricio Dall Onder <londer11@icloud.com>
2023-09-15 16:41:10 -0300

    Update UpdateProfessorData.tsx

--- src/pages/UpdateProfessorData.tsx
+++ src/pages/UpdateProfessorData.tsx
@@ -22,6 +22,12 @@ import { fieldsSessao1, fieldsSessao2 } from "../utils/constants";
 import { ref, remove, update } from "firebase/database";
 import { db } from "../api/config";
 import { FormHeader } from "../components/formHeader";
+interface Record {
+  ata: string;
+  funcao: string;
+  periodo: string;
+  turmas: string;
+}
 
 export const UpdateProfessorData = () => {
   const { allProfsData } = useFormDataContext();
@@ -29,6 +35,7 @@ export const UpdateProfessorData = () => {
   const [selectedProfessor, setSelectedProfessor] = useState<string | null>(
     null
   );
+  const [currentRecords, setCurrentRecords] = useState<Record[]>([]);
 
   useEffect(() => {
     if (!selectedProfessor) {
@@ -43,7 +50,7 @@ export const UpdateProfessorData = () => {
       setValue("inicio", professor?.inicio);
       setValue("matricula", professor?.matricula);
 
-      if (professor) {
+      if (professor && professor.records) {
         for (let i = 0; i < professor.records.length; i++) {
           setValue(`periodo${i + 1}`, professor.records[i].periodo);
           setValue(`funcao${i + 1}`, professor.records[i].funcao);
@@ -51,6 +58,7 @@ export const UpdateProfessorData = () => {
           setValue(`ata${i + 1}`, professor.records[i].ata);
         }
       }
+      setCurrentRecords(professor?.records || []);
     }
   }, [setValue, allProfsData, selectedProfessor]);
 
@@ -64,7 +72,7 @@ export const UpdateProfessorData = () => {
       );
 
       // Reestruture os dados antes de atualizá-los
-      const records = professor?.records.map((_record, index) => {
+      const records = currentRecords.map((_record, index) => {
         return {
           ata: formData[`ata${index + 1}`],
           funcao: formData[`funcao${index + 1}`],
@@ -90,8 +98,8 @@ export const UpdateProfessorData = () => {
 
       update(ref(db, "professors/" + professor?.id), updatedData)
         .then(() => {
-          console.log("Data saved successfully.");
-          reset();
+          reset(); // resetando o formulário aqui
+          setCurrentRecords([]); // Também resetando os registros atuais
           alert("Dados atualizados com sucesso!");
         })
         .catch((error) => {
@@ -120,6 +128,46 @@ export const UpdateProfessorData = () => {
     }
   };
 
+  const onAddYear = () => {
+    setCurrentRecords((prevRecords) => [
+      ...prevRecords,
+      {
+        periodo: "",
+        funcao: "",
+        turmas: "",
+        ata: "",
+      },
+    ]);
+  };
+
+  const onRemoveYear = (index: number) => {
+    setCurrentRecords((prevRecords) => {
+      const newRecords = [...prevRecords];
+      newRecords.splice(index, 1);
+      return newRecords;
+    });
+
+    // Aqui, há o reajuste dos valores dos campos no formulário.
+    for (let i = index; i < currentRecords.length - 1; i++) {
+      setValue(`ata${i + 1}`, currentRecords[i + 1].ata);
+      setValue(`funcao${i + 1}`, currentRecords[i + 1].funcao);
+      setValue(`periodo${i + 1}`, currentRecords[i + 1].periodo);
+      setValue(`turmas${i + 1}`, currentRecords[i + 1].turmas);
+    }
+    // E finalmente,  há a removação o último conjunto de campos, pois ele não é mais necessário.
+    setValue(`ata${currentRecords.length}`, "");
+    setValue(`funcao${currentRecords.length}`, "");
+    setValue(`periodo${currentRecords.length}`, "");
+    setValue(`turmas${currentRecords.length}`, "");
+  };
+
+
+
+  useEffect(() => {
+    console.log(currentRecords);
+  }, [currentRecords]);
+
+
   return (
     <Container>
       <Box sx={BoxStyleCadastro}>
@@ -130,19 +178,18 @@ export const UpdateProfessorData = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <Select
             value={selectedProfessor ? selectedProfessor : ""}
-            onChange={(event) =>
-              setSelectedProfessor(event.target.value as string)
-            }
+            onChange={(event) => setSelectedProfessor(event.target.value as string)}
           >
             <MenuItem value="">
               <em>-</em>
             </MenuItem>
-            {allProfsData.map((professor, index) => (
+            {allProfsData.sort((a, b) => a.nome_professor.localeCompare(b.nome_professor)).map((professor, index) => (
               <MenuItem key={index} value={professor.nome_professor}>
                 {professor.nome_professor}
               </MenuItem>
             ))}
           </Select>
+
           <List sx={ListStyle}>
             <Typography sx={TituloSecaoStyle}>
               Seção 1 - Identificação do Professor:
@@ -169,45 +216,93 @@ export const UpdateProfessorData = () => {
             <Typography sx={TituloSecaoStyle}>
               Seção 2 - Informações Documentais:
             </Typography>
-            {selectedProfessor &&
-              allProfsData
-                .find((prof) => prof.nome_professor === selectedProfessor)
-                ?.records.map((_record, professorIndex) => (
-                  <Grid container spacing={2} key={professorIndex}>
-                    {fieldsSessao2.map(({ id, label }) => (
-                      <Grid item xs={12} sm={3} key={id}>
-                        <TextField
-                          fullWidth
-                          id={`${id}${professorIndex + 1}`}
-                          variant="standard"
-                          label={label}
-                          {...register(`${id}${professorIndex + 1}`)}
-                        />
-                      </Grid>
-                    ))}
-                  </Grid>
-                ))}
+            {currentRecords.map((record, professorIndex) => (
+              <div key={record.ata + record.periodo}>
+                {" "}
+
+                <Grid container spacing={2}>
+                  {fieldsSessao2.map(({ id, label }) => (
+                    <Grid item xs={12} sm={6} key={id}>
+                      <TextField
+                        fullWidth
+                        id={`${id}${professorIndex + 1}`}
+                        variant="standard"
+                        label={label}
+                        {...register(`${id}${professorIndex + 1}`)}
+                        sx={{ fontSize: '6px' }}
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+                {/* Aqui, adicionamos o botão de remover */}
+                <Box sx={{ display: "flex", justifyContent: "center", marginTop: "5px", marginBottom: "15px" }}>
+                  {currentRecords.length === 1 ? (
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Button
+                        sx={{ marginTop: "5px", marginBottom: "5px" }}
+                        variant="contained"
+                        color="error"
+                        onClick={() => onRemoveYear(professorIndex)}
+                        disabled
+                      >
+                        Remover Ano
+                      </Button>
+                      <Typography>
+                        Esse campo não pode ser removido pois é necessario ter
+                        no mínimo 1 registro de trabalho
+                      </Typography>
+                    </Box>
+                  ) : (
+                    <Button
+                      sx={{ marginTop: "5px", marginBottom: "5px" }}
+                      variant="contained"
+                      color="error"
+                      onClick={() => onRemoveYear(professorIndex)}
+                    >
+                      Remover Ano
+                    </Button>
+                  )}
+                </Box>
+              </div>
+            ))}
           </List>
-          <Button
-            variant="contained"
-            sx={{ marginTop: "6px" }}
-            type="submit"
-            color="primary"
-            disabled={!selectedProfessor} // se selectedProfessor for null ou vazio, desabilita o botão
-          >
-            {!selectedProfessor
-              ? "selecione um professor."
-              : "Atualizar dados"}
-          </Button>
-          <Button
-            variant="contained"
-            sx={{ marginTop: "6px", marginRight: "10px" }}
-            color="error"
-            disabled={!selectedProfessor} // se selectedProfessor for null ou vazio, desabilita o botão
-            onClick={onDeleteProfessor}
-          >
-            Deletar Professor
-          </Button>
+          <Box sx={{ display: "flex", gap: "10px", justifyContent: "center" }}>
+            <Button
+              variant="contained"
+              sx={{ marginTop: "6px" }}
+              type="submit"
+              color="primary"
+              disabled={!selectedProfessor}
+            >
+              {!selectedProfessor
+                ? "selecione um professor."
+                : "Atualizar dados"}
+            </Button>
+            <Button
+              variant="contained"
+              sx={{ marginTop: "6px", marginRight: "10px" }}
+              color="error"
+              disabled={!selectedProfessor}
+              onClick={onDeleteProfessor}
+            >
+              Deletar Professor
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{ marginTop: "6px", marginRight: "10px" }}
+              onClick={onAddYear}
+              disabled={!selectedProfessor}
+            >
+              Adicionar Ano
+            </Button>
+          </Box>
         </form>
       </Box>
     </Container>
